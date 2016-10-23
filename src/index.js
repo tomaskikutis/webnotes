ad36bb43a3011d8df888625c5c6269b812655a1f
Tomas Kikutis <t.kikutis@gmail.com>
2016-10-23 16:33:18 +0300

    Add service worker for offline usage

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import Store from './Store';
 import App from './App';
 import './index.css';
 
+navigator.serviceWorker.register('serviceWorker.js');
+
 ReactDOM.render(
   <Store App={App}></Store>,
   document.getElementById('root')
