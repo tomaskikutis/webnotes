fcf3374b958b51fc7071a2bce0852aac11ad1d9d
Tomas Kikutis <t.kikutis@gmail.com>
2016-10-23 11:31:38 +0300

    Add edit note button

--- src/NotesList.js
+++ src/NotesList.js
@@ -10,7 +10,14 @@ class NotesList extends Component {
 						return new Date(b.lastUpdatedOn) - new Date(a.lastUpdatedOn);
 					})
 					.map(function(note){
-						return <li className="list-group-item" key={note.id}>{note.id}: {note.text}</li>;
+						return (
+							<li className="list-group-item clearfix" key={note.id}>
+								{note.id}: {note.text}
+								<button type="button" className="btn btn-default pull-right" aria-label="Edit note">
+									<span className="glyphicon glyphicon-edit" aria-hidden="true"></span>
+								</button>
+							</li>
+						);
 					})
 				}
 			</ul>
