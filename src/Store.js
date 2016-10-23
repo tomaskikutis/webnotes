30419f3183a91665a021f562e1be0dca3ca1aaf3
Tomas Kikutis <t.kikutis@gmail.com>
2016-10-23 10:11:41 +0300

    Persist state in localStorage

--- src/Store.js
+++ src/Store.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 
+const localStorageStateKey = 'state';
+
 class Store extends Component {
   constructor(props) {
     super(props);
-    this.state = {
+
+    var savedState = localStorage.getItem(localStorageStateKey);
+
+    this.state = savedState !== null ? JSON.parse(savedState) : {
       notesLastId: 0,
       notes: []
     };
+
     this.actions = {
       createNote: this.actionCreateNote.bind(this)
     };
   }
+  setStateAndPersist(nextState){
+    this.setState(nextState, this.persistState);
+  }
+  persistState(){
+    localStorage.setItem(localStorageStateKey, JSON.stringify(this.state));
+  }
   actionCreateNote(text){
-    this.setState({
+    this.setStateAndPersist({
       notes: this.state.notes.concat({id: ++this.state.notesLastId, text: text})
     });
   }
